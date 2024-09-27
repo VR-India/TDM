d6c53fbcb084628c9ea84f8d427448104f9b0ecd
Santosh Kedari <santoshkedari@inucom.org>
2024-09-27 18:41:52 +0530

    backup2

--- Assets/Scripts/Ghost.cs
+++ Assets/Scripts/Ghost.cs
@@ -6,14 +6,17 @@ using UnityEngine;
 
 public class Ghost : MonoBehaviour
 {
-    private void OnTriggerEnter(Collider other)
+    private void OnTriggerStay(Collider other)
     {
-        if(other.gameObject.tag == "survexical collar" && !other.GetComponent<Grabber>().HeldGrabbable)
+        if(other.gameObject.tag == "survexical collar" && !other.GetComponent<Grabbable>().BeingHeld)
         {
             other.transform.parent = transform.parent;
             other.transform.position = transform.position;
             other.transform.rotation = transform.rotation;
+            other.GetComponent<CapsuleCollider>().enabled = false;
             Debug.Log("triggered");
         }
+        else
+            Debug.Log("didn't enter if condition");
     }
 }
